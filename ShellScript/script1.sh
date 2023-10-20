49cfca36570152d0aa7dc1733f1e9b9dfcadaf4f
AmoghBari <145555795+AmoghBari@users.noreply.github.com>
2023-10-20 16:38:28 +0530

    Update script.sh

--- ShellScript/script1.sh
+++ ShellScript/script1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Define the valid options for each input
+# Defining the Valid Options for Inputs
 valid_components=("INGESTOR" "JOINER" "WRANGLER" "VALIDATOR")
 valid_scales=("MID" "HIGH" "LOW")
 valid_views=("Auction" "Bid")
 valid_count_regex='^[0-9]$'  # Regex to match a single digit
 
-# Function to validate user input against valid options
+# Function to Validate User Inputs
 validate_input() {
   local input="$1"
   shift
@@ -14,20 +14,20 @@ validate_input() {
 
   for option in "${valid_options[@]}"; do
     if [ "$input" == "$option" ]; then
-      return 0  # Valid input
+      return 0
     fi
   done
 
-  return 1  # Invalid input
+  return 1
 }
 
-# Main script
+# Main Script
 read -p "Enter Component Name [INGESTOR/JOINER/WRANGLER/VALIDATOR]: " component
 read -p "Enter Scale [MID/HIGH/LOW]: " scale
 read -p "Enter View [Auction/Bid]: " view
 read -p "Enter Count [single digit number]: " count
 
-# Validate user inputs
+# Validating the User Inputs
 if ! validate_input "$component" "${valid_components[@]}"; then
   echo "Invalid Component Name. Please choose from the provided options."
   exit 1
@@ -48,7 +48,7 @@ if ! [[ $count =~ $valid_count_regex ]]; then
   exit 1
 fi
 
-# Create the new line
+# Creating a New Line
 new_line="$view ; $scale ; $component ; ETL ; vdopia-etl= $count"
 
 # Use sed to replace the first line in the sig.conf file
